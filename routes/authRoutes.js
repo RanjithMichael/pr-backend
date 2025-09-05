d9e16924bfe6b4623c6b7eddd94ce798c843258f
RanjithMichael <branjithmichael@gmail.com>
2025-09-05 19:53:34 +0530

    updated authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,9 +8,11 @@ import {
 
 const router = express.Router();
 
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/forgot-password", forgotPassword);
 router.put("/reset-password/:token", resetPassword);
 
 export default router;
+
