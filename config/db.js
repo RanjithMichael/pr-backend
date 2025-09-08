b8fa18fccf4adc7385f6ae383beae4ef016957ae
RanjithMichael <branjithmichael@gmail.com>
2025-09-08 18:51:16 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`❌ Error: ${error.message}`);
+    console.log("Connecting to:", process.env.MONGO_URI); 
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("Error connecting MongoDB:", err.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
+
+
